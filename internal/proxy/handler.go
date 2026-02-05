11a802d616fd351573f8c939642a3c9216297047
Lachlan Wisdom <its-wizza@proton.me>
2026-02-05 22:37:13 +1100

    Start of Exit

--- internal/proxy/handler.go
+++ internal/proxy/handler.go
@@ -6,12 +6,21 @@ import (
 	"net/url"
 )
 
-// TargetResolver takes a request and returns the target URL to which the request should be proxied.
+// TargetResolver is a function type that takes an incoming HTTP request
+// and returns a target URL to which the request should be proxied.
 type TargetResolver func(*http.Request) (*url.URL, error)
 
+type Exit string
+
+// ExitSelector is a function type that takes an incoming HTTP request and
+// a target URL, and returns an Exit value indicating the selected exit point.
+type ExitSelector func(*http.Request, *url.URL) (Exit, error)
+
 // NewDynamicTargetHandler returns an http.Handler that resolves the target
-// for each incoming request using the provided TargetResolver. The request is modified to point to the resolved target and delegates handling
-// to the provided proxyHandler.
+// for each incoming request using the provided TargetResolver. The incoming
+// request is rewritten to point to the resolved target (URL, Host, and
+// RequestURI) and then delegated to the provided proxyHandler for actual
+// proxying.
 func NewDynamicTargetHandler(resolver TargetResolver, proxyHandler http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
 		log.Printf("[handler] %s %s from %s", req.Method, req.RequestURI, req.RemoteAddr)
@@ -22,6 +31,8 @@ func NewDynamicTargetHandler(resolver TargetResolver, proxyHandler http.Handler)
 			http.Error(writer, "Could not resolve target", http.StatusBadRequest)
 			return
 		}
+
+		// Raise error if target is not absolute URL
 		if !target.IsAbs() || target.Host == "" {
 			log.Printf("[handler] invalid target URL: %s", target.String())
 			http.Error(writer, "Target must be absolute URL", http.StatusBadRequest)
@@ -30,10 +41,10 @@ func NewDynamicTargetHandler(resolver TargetResolver, proxyHandler http.Handler)
 
 		log.Printf("[handler] resolved target: %s", target.String())
 
-		// Set target URL
+		// Modify the request to point to the resolved target
 		req.URL = target
 		req.Host = target.Host
-		req.RequestURI = ""
+		req.RequestURI = "" // RequestURI must be empty when making client requests
 
 		log.Printf("[handler] proxying to %s", req.URL.String())
 
